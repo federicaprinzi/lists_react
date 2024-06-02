87299d1665a2b3aaa2695c3704ebe6b5a1c824ef
Federica <fedecprinzi@gmail.com>
2024-06-02 18:42:41 +0200

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import "./App.css";
-import Counter from "./components/Counter";
+import Colors from "./components/Colors";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Counter:</h1>
-      <Counter initialValue={0} />
-    </div>
-  );
+  const colors = [
+    {
+      id: 0,
+      name: "purple",
+    },
+    {
+      id: 1,
+      name: "blue",
+    },
+    {
+      id: 2,
+      name: "green",
+    },
+    {
+      id: 3,
+      name: "yellow",
+    },
+    {
+      id: 4,
+      name: "orange",
+    },
+    {
+      id: 4,
+      name: "red",
+    },
+  ];
+
+  return <Colors colors={colors} />;
 };
 
 export default App;
